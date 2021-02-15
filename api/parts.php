4716a2130bea48e892391ec80e41e75310ef2837
vnguyen <vnguyen@LKR-NB-2020-14.local>
2021-02-15 02:12:37 -0500

    Update login.php, part-api.php, and 3 more files...

--- api/parts.php
+++ api/parts.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
